e26bac572ca4df1db1b55ff169d890f9d435fc0b
Юлия Юркина <parma2903@gmail.com>
2024-02-21 11:31:07 +0100

    Разбивает на компоненты

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,14 @@
-export const Setting = {
+const Setting = {
   OfferCardsCount: 312,
 } as const;
+
+const CITIES: string[] = [
+  'Paris',
+  'Cologne',
+  'Brussels',
+  'Amsterdam',
+  'Hamburg',
+  'Dusseldorf'
+];
+
+export { Setting, CITIES };
