5f230b4a0586981043417eccdc95aaf947671897
Юлия Юркина <parma2903@gmail.com>
2024-02-28 16:04:56 +0100

    Отрисывывает компоненты, прокидывая пропсы

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {Setting} from './const';
+import { AllOffers } from './mocks/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App
       offerCardsCount = {Setting.OfferCardsCount}
+      offers={AllOffers}
     />
   </React.StrictMode>
 );
