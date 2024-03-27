d40ca86dadb1ad4e1dc03d0038dbf9b8926052d4
Юлия Юркина <parma2903@gmail.com>
2024-03-27 12:19:01 +0100

    Обновит страницу LoginScreen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,28 @@
 import { Link } from 'react-router-dom';
+import {useRef, FormEvent} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../hooks/useApp';
+import { loginAction } from '../store/api-actions';
+import { AppRoute } from '../const';
 
 function LoginScreen(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleSumit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      dispatch(loginAction({
+        login: loginRef.current.value,
+        password: passwordRef.current.value
+      }));
+    }
+  };
+
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -24,13 +46,15 @@ function LoginScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post" onSubmit={handleSumit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input
+                  ref={loginRef}
                   className="login__input form__input"
                   type="email"
                   name="email"
+                  id="email"
                   placeholder="Email"
                   required
                 />
@@ -38,6 +62,7 @@ function LoginScreen(): JSX.Element {
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
                 <input
+                  ref={passwordRef}
                   className="login__input form__input"
                   type="password"
                   name="password"
@@ -45,7 +70,11 @@ function LoginScreen(): JSX.Element {
                   required
                 />
               </div>
-              <button className="login__submit form__submit button" type="submit">
+              <button
+                className="login__submit form__submit button"
+                type="submit"
+                onClick={() => navigate(AppRoute.Main)}
+              >
                 Sign in
               </button>
             </form>
