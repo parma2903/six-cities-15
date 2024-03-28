eaa61ab200c3a8c941653fcfd142d50d0ea18342
Юлия Юркина <parma2903@gmail.com>
2024-03-28 10:41:09 +0100

    Изменяет header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom';
+import SignOutButton from '../signOutButton/signOutButton';
+import { useAppSelector } from '../../hooks/useApp';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { NavLink } from 'react-router-dom';
 
 function Header(): JSX.Element {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+  const isLoggedIn = authorizationStatus === AuthorizationStatus.Auth;
+
   return (
     <header className="header">
       <div className="container">
@@ -17,25 +24,36 @@ function Header(): JSX.Element {
             </Link>
           </div>
           <nav className="header__nav">
-            <ul className="header__nav-list">
+            {isLoggedIn ? (
+              <ul className="header__nav-list">
+                <li className="header__nav-item">
+                  <NavLink
+                    className="header__nav-link header__nav-link--profile"
+                    to={AppRoute.Favorites}
+                  >
+                    <div className="header__avatar-wrapper user__avatar-wrapper"
+                      style={{ backgroundImage: user?.avatarUrl }}
+                    >
+                    </div>
+                    <span className="header__user-name user__name">{user?.name}</span>
+                    <span className="header__favorite-count">3</span>
+                  </NavLink>
+                </li>
+                <li className="header__nav-item">
+                  <SignOutButton />
+                </li>
+              </ul>
+            ) : (
               <li className="header__nav-item user">
                 <Link
                   className="header__nav-link header__nav-link--profile"
-                  to="#"
+                  to={AppRoute.Login}
                 >
                   <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                  <span className="header__user-name user__name">
-                    Oliver.conner@gmail.com
-                  </span>
-                  <span className="header__favorite-count">3</span>
+                  <span className="header__login">Sign in</span>
                 </Link>
               </li>
-              <li className="header__nav-item">
-                <Link className="header__nav-link" to="#">
-                  <span className="header__signout">Sign out</span>
-                </Link>
-              </li>
-            </ul>
+            )}
           </nav>
         </div>
       </div>
---
+++ src/components/signOutButton/signOutButton.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import { logoutAction } from '../../store/api-actions';
+import { useAppDispatch } from '../../hooks/useApp';
+import { AppRoute } from '../../const';
+
+function SignOutButton(): JSX.Element {
+  const dispatch = useAppDispatch();
+  return (
+    <Link
+      className="header__nav-link"
+      onClick={(evt) => {
+        evt.preventDefault();
+        dispatch(logoutAction());
+      }}
+      to={AppRoute.Login}
+    >
+      <span className="header__signout">Sign out</span>
+    </Link>
+  );
+}
+
+export default SignOutButton;
